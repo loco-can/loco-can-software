c2aef1f329d288b4677309c1b8ba2cfdb2a81120
loco-can <126269438+loco-can@users.noreply.github.com>
2026-06-22 21:49:55 +0200

    Aktualisieren von parameter.h

--- src/module/parameter.h
+++ src/module/parameter.h
@@ -16,23 +16,47 @@
 
 // =======================================
 // common module data
-  struct MODULE_TYPE {
-    char type; // module type id
-	   int software_version;
-	   int hardware_version;
+	struct MODULE_TYPE {
+		char type; // module type id
+		int software_version;
+		int hardware_version;
   };
+
 
 // =======================================
 // controller module parameters definition
 #ifdef CONTROLLER_MODULE_CLASS
 
-  struct PARAM_CONTROLLER {
-    MODULE_TYPE type;
-	   int mains_points[ANALOGSWITCH_MAX_POS]; // settings for mains analogswitch
-	   int dir_points[3]; // settings of direction analogswitch
-	   char status_mode; // led, oled status type
-	   char drive_mode; // drive mode: single, drive/break, speed/power/break
-  }
+	struct PARAM_CONTROLLER {
+		MODULE_TYPE type;
+		int mains_points[ANALOGSWITCH_MAX_POS]; // settings for mains analogswitch
+		int dir_points[3]; // settings of direction analogswitch
+		char status_mode; // led, oled status type
+		char drive_mode; // drive mode: single, drive/break, speed/power/break
+	}
+
+#endif
+
+// =======================================
+
+
+// =======================================
+// drive paramerer definition
+
+#ifdef DRIVE_MODULE_CLASS
+
+	struct PARAM_DRIVE {
+		MODULE_TYPE type;
+		int mains_points[ANALOGSWITCH_MAX_POS]; // settings for mains analogswitch
+		int dir_points[3]; // settings of direction analogswitch
+		char status_mode; // led, oled status type
+		char drive_mode; // drive mode: single, drive/break, speed/power/break
+		
+		int paired; // uuid of paired controller
+		int multi_traction; // 0=single, 1=leading engine, 2=supporting engine
+		int tacho_pulses; // tacho pulses per meter
+		int battery_type; // battery type (1x12V/24V, 2x12V in serie) 
+	}
 
 #endif
 
@@ -44,13 +68,13 @@
 
 #ifdef ELECTRIC_MODULE_CLASS
 
-  struct PARAM_ELECTRIC {
-    MODULE_TYPE type;
-    int paired; // uuid of paired controller
-    int multi_traction; // 0=single, 1=leading engine, 2=supporting engine
-    int tacho_pulses; // tacho pulses per meter
-    int battery_type; // battery type (1x12V/24V, 2x12V in serie) 
-  }
+	struct PARAM_ELECTRIC {
+		MODULE_TYPE type;
+		int paired; // uuid of paired controller
+		int multi_traction; // 0=single, 1=leading engine, 2=supporting engine
+		int tacho_pulses; // tacho pulses per meter
+		int battery_type; // battery type (1x12V/24V, 2x12V in serie) 
+	}
 
 #endif
 
