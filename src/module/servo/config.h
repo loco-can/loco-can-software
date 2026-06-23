0e469a808b55c671bd61c02b2980b1d1d712742f
loco-can <126269438+loco-can@users.noreply.github.com>
2026-06-23 09:43:10 +0200

    Update servo configuration for version 2.0 and 2.1

--- src/module/servo/config.h
+++ src/module/servo/config.h
@@ -22,7 +22,7 @@
  * The module version is set in the config.h file
  */
 
-#if MODULE_VERSION == V_1_0
+#if SERVO_MODULE_VERSION == V_2_0 || SERVO_MODULE_VERSION == V_2_1
 
 	// ======================================
 	// BASIC SETTINGS
@@ -41,159 +41,23 @@
 	// ======================================
 
 	// FUNCTION PARAMETERS
-	#define SERVO_MAINS_PORT A1
-	#define SERVO_DIR_PORT A2
-	// #define SERVO_HORN_PORT 5
-	// #define SERVO_HORN2_PORT 9
-	#define SERVO_LIGHT_PORT A3
-	// #define SERVO_LIGHT2_PORT A2
+	// servo output
+	#define SERVO_1 2
+	#define SERVO_2 3
+	#define SERVO_3 4
+	#define SERVO_4 5
 
-	#define ANALOGSWITCH_MAX_POS 8
+	// pulse input
+	#define PULSE A6
 
+	// VOLTAGE 
+	#define V1 A1
+	#define V2 A2
+	#define V3 A3
+	#define V4 A4
 
-	// FUNCTION SWITCH
-	// #define SERVO_INSTRUMENT_LIGHT_PORT 4
-
-	// FUNCTION GAUGE
-	// #define SERVO_BATTERY_VOLTAGE_PORT 6
-	// #define SERVO_MOTOR_VOLTAGE_PORT 7
-	// #define SERVO_CURRENT_PORT 8
-
-	#define SERVO_DRIVE_MODE SINGLE
-	#define SERVO_DRIVE_PORT A0
-	// #define SERVO_BREAK_PORT A1
-	// #define SERVO_POWER_PORT A2
-
-
-/*
- * CONTROLLER ADAPTER with universal module V2.0
- * (has wrong ICP pinout)
- */
-#elif MODULE_VERSION == V_2_0
-
-	// ======================================
-	// BASIC SETTINGS
-	// ======================================
-	#define MODULE_ARCH_AVR
-	#define PLATFORM_ANALOG_RESOLUTION 1024
-
-	#define CAN_RX 10
-	#define CAN_TX 2
-	#define CAN_STATUS_LED 5
-	#define CAN_MAX_FILTER 8
-	#define CAN_BUFFER_SIZE 8
-
-	// ======================================
-	// INCLUDED FUNCTIONS
-	// ======================================
-
-	// FUNCTION PARAMETERS
-	#define SERVO_MAINS_PORT A4
-	#define SERVO_DIR_PORT A3
-	#define SERVO_HORN_PORT 5
-	#define SERVO_HORN2_PORT 9
-	#define SERVO_LIGHT_PORT A5
-	#define SERVO_LIGHT2_PORT A2
-
-	#define ANALOGSWITCH_MAX_POS 8
-
-
-	// FUNCTION SWITCH
-	#define SERVO_INSTRUMENT_LIGHT_PORT 4
-
-	// FUNCTION GAUGE
-	#define SERVO_BATTERY_VOLTAGE_PORT 6
-	#define SERVO_MOTOR_VOLTAGE_PORT 7
-	#define SERVO_CURRENT_PORT 8
-
-	#define SERVO_DRIVE_MODE SINGLE
-	#define SERVO_DRIVE_PORT A0
-	#define SERVO_BREAK_PORT A1
-	#define SERVO_POWER_PORT A2
-
-
-/*
- * CONTROLLER ADAPTER with universal module V2.1
- */
- #elif MODULE_VERSION == V_2_1
-
-	// ======================================
-	// BASIC SETTINGS
-	// ======================================
-	#define MODULE_ARCH_AVR
-	#define PLATFORM_ANALOG_RESOLUTION 1024
-
-	#define CAN_RX 10
-	#define CAN_TX 2
-	#define CAN_STATUS_LED 5
-	#define CAN_MAX_FILTER 8
-	#define CAN_BUFFER_SIZE 8
-
-
-	// ======================================
-	// INCLUDED FUNCTIONS
-	// ======================================
-
-	/* define corresponding parameters */ 
-	// switches
-	#define SERVO_MAINS_PORT A4
-	#define SERVO_DIR_PORT A3
-	#define SERVO_HORN_PORT 5
-	#define SERVO_HORN2_PORT 9
-	#define SERVO_LIGHT_PORT A5
-	#define SERVO_LIGHT2_PORT A2
-
-	#define ANALOGSWITCH_MAX_POS 8
-
-	#define SERVO_DRIVE_MODE SINGLE
-	#define SERVO_DRIVE_PORT A0
-	#define SERVO_BREAK_PORT A1
-	// #define SERVO_POWER_PORT A2
-
-	// status display method
-	#define CONTROLER_STATUS_MODE LED
-
-	#if SERVO_STATUS_MODE == LED
-		#define SERVO_STATUS_RED_PORT 2
-		#define SERVO_STATUS_GREEN_PORT 3
-	#elif SERVO_STATUS_MODE == OLED
-		// supported OLED displays
-	#endif
-
-	// FUNCTION SWITCH
-	#define SERVO_INSTRUMENT_LIGHT_PORT 4
-
-	// FUNCTION GAUGE
-	#define SERVO_BATTERY_VOLTAGE_PORT 6
-	// #define SERVO_MOTOR_VOLTAGE_PORT 7
-	#define SERVO_CURRENT_PORT 8
-
-
-/*
- * ESP32S3 based version
- * not yet implemented, only for debugging use
- */
-#elif MODULE_VERSION == V_3_0
-
-	#define MODULE_ARCH_ESP32
-	#define PLATFORM_ANALOG_RESOLUTION 2048
-
-
-	// CAN ports
-	#define CAN_RX 17
-	#define CAN_TX 18
-	#define CAN_STATUS_LED 46
-	#define CAN_MAX_FILTER 16
-	#define CAN_BUFFER_SIZE 16
-
-
-	// ======================================
-	// INCLUDED FUNCTIONS
-	// ======================================
-
-	#define ANALOGSWITCH_MAX_POS 8
-	// CAN LED
-
+#else
+	#error "No valid board version selected"
 #endif
 
 /*
