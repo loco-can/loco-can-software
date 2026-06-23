34ff0769d1489be428bcf7840bf32483dba1b4df
loco-can <126269438+loco-can@users.noreply.github.com>
2026-06-23 08:47:29 +0200

    Refactor parameter structures to use uint16 types
    switch module definition added

--- src/module/parameter.h
+++ src/module/parameter.h
@@ -16,24 +16,24 @@
 
 // =======================================
 // common module data
-	struct MODULE_TYPE {
-		char type; // module type id
-		int software_version;
-		int hardware_version;
-  };
+struct MODULE_TYPE {
+	uint8_t type; // module type id
+	uint16_t software_version;
+	uint16_t hardware_version;
+};
 
 
 // =======================================
-// controller module parameters definition
+// CONTROLLER module parameters definition
 #ifdef CONTROLLER_MODULE_CLASS
 
-	struct PARAM_CONTROLLER {
-		MODULE_TYPE type;
-		int mains_points[ANALOGSWITCH_MAX_POS]; // settings for mains analogswitch
-		int dir_points[3]; // settings of direction analogswitch
-		char status_mode; // led, oled status type
-		char drive_mode; // drive mode: single, drive/break, speed/power/break
-	}
+struct PARAM_CONTROLLER {
+	MODULE_TYPE type;
+	uint16_t mains_points[ANALOGSWITCH_MAX_POS]; // settings for mains analogswitch
+	uint16_t dir_points[3]; // settings of direction analogswitch
+	uint8_t status_mode; // led, oled status type
+	uint8_t drive_mode; // drive mode: single, drive/break, speed/power/break
+};
 
 #endif
 
@@ -41,22 +41,22 @@
 
 
 // =======================================
-// drive paramerer definition
+// DRIVE parameters definition
 
 #ifdef DRIVE_MODULE_CLASS
 
-	struct PARAM_DRIVE {
-		MODULE_TYPE type;
-		int mains_points[ANALOGSWITCH_MAX_POS]; // settings for mains analogswitch
-		int dir_points[3]; // settings of direction analogswitch
-		char status_mode; // led, oled status type
-		char drive_mode; // drive mode: single, drive/break, speed/power/break
-		
-		int paired; // uuid of paired controller
-		int multi_traction; // 0=single, 1=leading engine, 2=supporting engine
-		int tacho_pulses; // tacho pulses per meter
-		int battery_type; // battery type (1x12V/24V, 2x12V in serie) 
-	}
+struct PARAM_DRIVE {
+	MODULE_TYPE type;
+	uint16_t mains_points[ANALOGSWITCH_MAX_POS]; // settings for mains analogswitch
+	uint16_t dir_points[3]; // settings of direction analogswitch
+	uint8_t status_mode; // led, oled status type
+	uint8_t drive_mode; // drive mode: single, drive/break, speed/power/break
+	
+	uint16_t paired; // uuid of paired controller
+	uint16_t multi_traction; // 0=single, 1=leading engine, 2=supporting engine
+	uint16_t tacho_pulses; // tacho pulses per meter
+	uint16_t battery_type; // battery type (1x12V/24V, 2x12V in serie) 
+};
 
 #endif
 
@@ -64,17 +64,68 @@
 
 
 // =======================================
-// electro locomotive paramerer definition
+// electro locomotive parameters definition
 
 #ifdef ELECTRIC_MODULE_CLASS
 
-	struct PARAM_ELECTRIC {
-		MODULE_TYPE type;
-		int paired; // uuid of paired controller
-		int multi_traction; // 0=single, 1=leading engine, 2=supporting engine
-		int tacho_pulses; // tacho pulses per meter
-		int battery_type; // battery type (1x12V/24V, 2x12V in serie) 
-	}
+struct PARAM_ELECTRIC {
+	MODULE_TYPE type;
+	uint16_t paired; // uuid of paired controller
+	uint16_t multi_traction; // 0=single, 1=leading engine, 2=supporting engine
+	uint16_t tacho_pulses; // tacho pulses per meter
+	uint16_t battery_type; // battery type (1x12V/24V, 2x12V in serie) 
+};
+
+#endif
+
+// =======================================
+
+
+// =======================================
+// sensor parameters definition
+
+#ifdef SENSOR_MODULE_CLASS
+
+struct PARAM_SENSOR {
+	MODULE_TYPE type;
+};
+
+#endif
+
+// =======================================
+
+
+// =======================================
+// servo parameters definition
+
+#ifdef SERVO_MODULE_CLASS
+
+struct PARAM_SERVO {
+	MODULE_TYPE type;
+};
+
+#endif
+
+// =======================================
+
+
+// =======================================
+// switch parameters definition
+
+#ifdef SWITCH_MODULE_CLASS
+
+struct SWITCH_MAP {
+	uint8_t port; // output port
+	uint16_t can_id; // message id to use for switching
+	uint8_t byte; // byte index of message
+	uint8_t bit; // bit to be used to switch
+	bool invert; // invert bit for switch state
+};
+
+struct PARAM_SWITCH {
+	MODULE_TYPE type;
+	SWITCH_MAP output[SWITCH_MAX_PORTS];
+};
 
 #endif
 
