05a5fbf180ecb2804bb1843ec1f5f4e8a0670a15
loco-can <126269438+loco-can@users.noreply.github.com>
2026-06-23 09:49:56 +0200

    Refactor parameter.h for module definitions and comments

--- src/module/parameter.h
+++ src/module/parameter.h
@@ -1,134 +1,117 @@
 /*
- * Loco-CAN module parameters
- *
+ * LOCO-DRIVE hardware selection file
+ * 
  * @author: Thomas H Winkler
  * @copyright: 2026
  * @lizence: GG0
  *
- * The parameter structures for all modules are defined in this file.
+ * Version 3.0
  */
 #pragma once
 
-#ifnded PARAMETER_H
-#define PARAMETER_H
-
-#include <inttypes.h>
-
-// =======================================
-// common module data
-struct MODULE_TYPE {
-	uint8_t type; // module type id
-	uint16_t software_version;
-	uint16_t hardware_version;
-};
+#ifndef HARDWARE_H
+#define HARDWARE_H
 
 
-// =======================================
-// CONTROLLER module parameters definition
-#ifdef CONTROLLER_MODULE_CLASS
-
-struct PARAM_CONTROLLER {
-	MODULE_TYPE type;
-	uint16_t mains_points[ANALOGSWITCH_MAX_POS]; // settings for mains analogswitch
-	uint16_t dir_points[3]; // settings of direction analogswitch
-	uint8_t status_mode; // led, oled status type
-	uint8_t drive_mode; // drive mode: single, drive/break, speed/power/break
-};
-
-#endif
-
-// =======================================
+/* ========================================================================
+ * HARDWARE SELECTION
+ *
+ * Select the harware module and its version to compile the sofware.
+ * Uncomment the corresponding line of the module and the used harware version. 
+ */
 
 
-// =======================================
-// DRIVE parameters definition
+/* ========================================================================
+ * CONTROLLER MODULE
+ *
+ * The controller module is based on a universal hardware combined with
+ * an adapter PCB with sockets for the potentiometer and switches.
+ * It is used to control locomotives with an electic or combution motor.
+ */
+#define MODULE CONTROLLER_MODULE
 
-#ifdef DRIVE_MODULE_CLASS
-
-struct PARAM_DRIVE {
-	MODULE_TYPE type;
-	uint16_t mains_points[ANALOGSWITCH_MAX_POS]; // settings for mains analogswitch
-	uint16_t dir_points[3]; // settings of direction analogswitch
-	uint8_t status_mode; // led, oled status type
-	uint8_t drive_mode; // drive mode: single, drive/break, speed/power/break
-	
-	uint16_t paired; // uuid of paired controller
-	uint16_t multi_traction; // 0=single, 1=leading engine, 2=supporting engine
-	uint16_t tacho_pulses; // tacho pulses per meter
-	uint16_t battery_type; // battery type (1x12V/24V, 2x12V in serie) 
-};
-
-#endif
-
-// =======================================
+/* ===== MODULE VERSIONS ===== */
+// #define CONTROLLER_MODULE_VERSION V_2_0
+#define CONTROLLER_MODULE_VERSION V_2_1
+/* ===================================================================== */
 
 
-// =======================================
-// electro locomotive parameters definition
+/* ========================================================================
+ * DRIVE MODULE
+ *
+ * The drive module is a 4-Q controller for electric locomotives with an
+ * integrated CAN bus interface. In addition potiometers and seitches as
+ * well as gauges can directly be connected, if the driver sits in the
+ * locomotive and teh module is also installed inside the vehicle. In this
+ * case the loco is controlled directly, but another loco can be controlled
+ * via the CAN bus.
+ */
+// #define MODULE DRIVE_MODULE
 
-#ifdef ELECTRIC_MODULE_CLASS
-
-struct PARAM_ELECTRIC {
-	MODULE_TYPE type;
-	uint16_t paired; // uuid of paired controller
-	uint16_t multi_traction; // 0=single, 1=leading engine, 2=supporting engine
-	uint16_t tacho_pulses; // tacho pulses per meter
-	uint16_t battery_type; // battery type (1x12V/24V, 2x12V in serie) 
-};
-
-#endif
-
-// =======================================
-
-
-// =======================================
-// sensor parameters definition
-
-#ifdef SENSOR_MODULE_CLASS
-
-struct PARAM_SENSOR {
-	MODULE_TYPE type;
-};
-
-#endif
-
-// =======================================
+/* ===== MODULE VERSIONS ===== */
+// #define DRIVE_MODULE_VERSION V_2_0
+/* ===================================================================== */
 
 
-// =======================================
-// servo parameters definition
+/* ========================================================================
+ * ELECTRIC MOTOR MODULE
+ *
+ * The motor module connects the CAN control with a electro motor driver.
+ * It has no power amplifier but only control lines to operate different
+ * motor drivers.
+ */
+// #define MODULE ELECTRIC_MODULE
 
-#ifdef SERVO_MODULE_CLASS
-
-struct PARAM_SERVO {
-	MODULE_TYPE type;
-};
-
-#endif
-
-// =======================================
+/* ===== MODULE VERSIONS ===== */
+// #define ELECTRIC_MODULE_VERSION V_2_0
+// #define ELECTRIC_MODULE_VERSION V_2_1
+/* ===================================================================== */
 
 
-// =======================================
-// switch parameters definition
+/* ========================================================================
+ * SENSOR MODULE
+ *
+ * The sensor module offers different sensors for electric values from
+ * voltage and electric current to pulse measuring for speed or rpm values.
+ */
+// #define MODULE SENSOR_MODULE
 
-#ifdef SWITCH_MODULE_CLASS
+/* ===== MODULE VERSIONS ===== */
+// #define SENSOR_MODULE_VERSION V_2_0
+// #define SENSOR_MODULE_VERSION V_2_1
+/* ===================================================================== */
 
-struct SWITCH_MAP {
-	uint8_t port; // output port
-	uint16_t can_id; // message id to use for switching
-	uint8_t byte; // byte index of message
-	uint8_t bit; // bit to be used to switch
-	bool invert; // invert bit for switch state
-};
 
-struct PARAM_SWITCH {
-	MODULE_TYPE type;
-	SWITCH_MAP output[SWITCH_PORT_COUNT];
-};
+/* ========================================================================
+ * SERVO MODULE
+ *
+ * The servo module can drive up to four analog model servo motors.
+ */
+// #define MODULE SERVO_MODULE
 
-#endif
+/* ===== MODULE VERSIONS ===== */
+// #define SERVO_MODULE_VERSION V_2_0
+// #define SERVO_MODULE_VERSION V_2_1
+/* ===================================================================== */
 
-// =======================================
+
+/* ========================================================================
+ * SWITCH MODULE
+ *
+ * The switch module offers six 5 Ampere outputs. The outputs can be mapped
+ * to all switching operations, that are defined in the CAN protocol.
+ * The default mapping is for switching lights. 
+ */
+// #define MODULE SWITCH_MODULE
+
+/* ===== MODULE VERSIONS ===== */
+// #define SWITCH_MODULE_VERSION V_2_0
+// #define SWITCH_MODULE_VERSION V_2_1
+/* ===================================================================== */
+
+/* ====================================================================== */
+// INCLUDE MODULE CLASS
+/* ====================================================================== */
+#include MODULE
 
 #endif
