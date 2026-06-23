07fc96ef86075fedc98dc76ed47a63e4e9afe2dd
loco-can <126269438+loco-can@users.noreply.github.com>
2026-06-23 09:32:55 +0200

    Update electric module configuration for version 2.1

--- src/module/electric/config.h
+++ src/module/electric/config.h
@@ -28,7 +28,9 @@
 	// BASIC SETTINGS
 	// ======================================
 	#define MODULE_ARCH_AVR
+
 	#define PLATFORM_ANALOG_RESOLUTION 1024
+	#define ANALOGSWITCH_MAX_POS 8
 
 	#define CAN_RX 17
 	#define CAN_TX 18
@@ -40,7 +42,36 @@
 	// INCLUDED FUNCTIONS
 	// ======================================
 
+	// DRIVE FUNCTION PARAMETERS
+	// CONTROLLER
+	#define DRIVE_PWM 15
+	#define DRIVE_BREAK 3
+	#define DRIVE_FORW 21
+	#define DRIVE_REV 2
+
+	// SENSORS
+	#define DRIVE_MOTOR_VOLTAGE_PLUS 4
+	#define DRIVE_MOTOR_VOLTAGE_MINUS 5
+
+#elif ELECTRIC_MODULE_VERSION == V_2_1
+
+	// ======================================
+	// BASIC SETTINGS
+	// ======================================
+	#define MODULE_ARCH_AVR
+
+	#define PLATFORM_ANALOG_RESOLUTION 1024
 	#define ANALOGSWITCH_MAX_POS 8
+
+	#define CAN_RX 17
+	#define CAN_TX 18
+	#define CAN_STATUS_LED 46
+	#define CAN_MAX_FILTER 8
+	#define CAN_BUFFER_SIZE 8
+
+	// ======================================
+	// INCLUDED FUNCTIONS
+	// ======================================
 
 	// DRIVE FUNCTION PARAMETERS
 	// CONTROLLER
