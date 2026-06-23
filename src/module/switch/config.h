171fcf7dbfb46a9979615bfcdbae10fc425e219d
loco-can <126269438+loco-can@users.noreply.github.com>
2026-06-23 09:22:17 +0200

    Refactor configuration for switch module versions

--- src/module/switch/config.h
+++ src/module/switch/config.h
@@ -25,44 +25,40 @@
  * The module version is set in the config.h file
  */
 
-// BOARD VERSION 1.0
-//
-//	4x analog in
-//	4x digital IO
-//	2x PWM
-//
-// Print V1.0 extension bus
-//
-// 10x1 pins
-//
-//	1	VCC
-//	2	D9	(PWM)
-//	3	D8
-//	4	D7
-//	5	D6	(PWM)
-//	6	A0
-//	7	A1
-//	8	A2
-//	9	A3
-//	10	GND
+#if SWITCH_MODULE_VERSION == V_1_0
 
-#if MODULE_VERSION == V_1_0
+	// BOARD VERSION 1.0
+	//
+	//	4x analog in
+	//	4x digital IO
+	//	2x PWM
+	//
+	// Print V1.0 extension bus
+	//
+	// 10x1 pins
+	//
+	//	1	VCC
+	//	2	D9	(PWM)
+	//	3	D8
+	//	4	D7
+	//	5	D6	(PWM)
+	//	6	A0
+	//	7	A1
+	//	8	A2
+	//	9	A3
+	//	10	GND
 
 	// ======================================
 	// BASIC SETTINGS
 	// ======================================
 	#define MODULE_ARCH_AVR
-	#define ANALOGSWITCH_MAX_POS 1024
+	#define PLATFORM_ANALOG_RESOLUTION 1024
 
 	#define CAN_RX 17
 	#define CAN_TX 18
 	#define CAN_STATUS_LED 46
 	#define CAN_MAX_FILTER 8
 	#define CAN_BUFFER_SIZE 8
-
-	// ======================================
-	// INCLUDED FUNCTIONS
-	// ======================================
 
 	// ======================================
 	// CONTROL FUNCTION PARAMETERS
@@ -77,90 +73,92 @@
 	#define LIGHT5 6
 	#define LIGHT6 7
 
-
 	// analog input
 	// CURRENT
 	#define C1 A0
 
-// Print V1.2 extension bus
-// 8x2 pins
-//
-//	1	VCC			2	D2
-//	3	A5			4	D3 (PWM)
-//	5	A4			6	D4
-//	7	A3			8	D5 (PWM)
-//	8	A2			10	D6 (PWM)
-//	11	A1			12	D7
-//	13	A0			14	D8
-//	15	D9 (PWM)	16	GND
 
-// big extension bus version
-// 9x2 pins
-//  ...
-//	17	+12V		18	GND
+#elif SWITCH_MODULE_VERSION == V_2_0
 
-#elif MODULE_VERSION == BOARD_V_1_2
+	// Print V1.2 extension bus
+	// 8x2 pins
+	//
+	//	1	VCC			2	D2
+	//	3	A5			4	D3 (PWM)
+	//	5	A4			6	D4
+	//	7	A3			8	D5 (PWM)
+	//	8	A2			10	D6 (PWM)
+	//	11	A1			12	D7
+	//	13	A0			14	D8
+	//	15	D9 (PWM)	16	GND
+
+	// big extension bus version
+	// 9x2 pins
+	//  ...
+	//	17	+12V		18	GND
 
 	// ======================================
 	// BASIC SETTINGS
 	// ======================================
 	#define MODULE_ARCH_AVR
-	#define ANALOGSWITCH_MAX_POS 1024
+	#define PLATFORM_ANALOG_RESOLUTION 1024
 
 	#define CAN_RX 17
 	#define CAN_TX 18
-	#define CAN_STATUS_LED 7
+	#define CAN_STATUS_LED 46
 	#define CAN_MAX_FILTER 8
 	#define CAN_BUFFER_SIZE 8
 
-	#define UI_MAX_VOLT 4
-	#define UI_MAX_CURR 1
-	#define UI_MAX_DIGITAL 8
-	#define UI_MAX_PWM 3
-
+	// ======================================
+	// CONTROL FUNCTION PARAMETERS
 	// digital intput/output
 	// not used
-	#define DIGI1 D2
-	#define DIGI2 D3
-	#define DIGI3 D4
-	#define DIGI4 D5
-	#define DIGI5 D6
-	#define DIGI6 D7
-	#define DIGI7 D8
-	#define DIGI8 D9
+	#define SWITCH_PORT_COUNT 6
 
-	#define PWM1 D3
-	#define PWM2 D6
-	#define PWM3 D9
-	#define PWM4 -1
-	#define PWM5 -1
-	#define PWM6 -1
+	#define LIGHT1 2
+	#define LIGHT2 3
+	#define LIGHT3 4
+	#define LIGHT4 5
+	#define LIGHT5 6
+	#define LIGHT6 7
 
 	// analog input
 	// CURRENT
 	#define C1 A0
-	#define C2 -1
-	#define C3 -1
-	#define C4 -1
-	#define C5 -1
-	#define C6 -1
 
-	// VOLTAGE 
-	#define V1 -1
-	#define V2 A1
-	#define V3 A2
-	#define V4 A3
-	#define V5 -1
-	#define V6 -1
+#elif SWITCH_MODULE_VERSION == V_2_1
 
-#elif BOARD_VERSION == BOARD_V_2_0
+	// Print V1.2 extension bus
+	// 8x2 pins
+	//
+	//	1	VCC			2	D2
+	//	3	A5			4	D3 (PWM)
+	//	5	A4			6	D4
+	//	7	A3			8	D5 (PWM)
+	//	8	A2			10	D6 (PWM)
+	//	11	A1			12	D7
+	//	13	A0			14	D8
+	//	15	D9 (PWM)	16	GND
 
-	#define CAN_STATUS_LED 8
-	#define CAN_STATUS_LED_WRITE 9
+	// big extension bus version
+	// 9x2 pins
+	//  ...
+	//	17	+12V		18	GND
 
-	#define ANALOGSWITCH_MAX_POS 1024
+	// ======================================
+	// BASIC SETTINGS
+	// ======================================
+	#define MODULE_ARCH_AVR
+	#define PLATFORM_ANALOG_RESOLUTION 1024
 
+	#define CAN_RX 17
+	#define CAN_TX 18
+	#define CAN_STATUS_LED 46
+	#define CAN_MAX_FILTER 8
+	#define CAN_BUFFER_SIZE 8
 
+	// ======================================
+	// CONTROL FUNCTION PARAMETERS
 	// digital intput/output
 	// not used
 	#define SWITCH_PORT_COUNT 6
